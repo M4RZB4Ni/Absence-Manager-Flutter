b02ec485c979e741c6b41a9578f1a60c7d214029
Marzbani <hamied.reza.m@gmail.com>
2024-04-21 02:58:15 +0330

    Docs: absence_manager_data_source.dart's comments are added
    Description: none
    
    Took 4 minutes

--- lib/domain/data_source/absence_manager_data_source.dart
+++ lib/domain/data_source/absence_manager_data_source.dart
@@ -2,7 +2,13 @@ import '../../app/base/api_result.dart';
 import '../../data/models/crew/crew_member.dart';
 import '../../data/models/leave_request/leave_request_model.dart';
 
-abstract class AbsenceManagerDataSource{
+// Defines an abstract class for absence management data operations.
+abstract class AbsenceManagerDataSource {
+  /// Retrieves a list of leave requests.
+  /// Returns a future of [ApiResult] containing a [LeaveRequestsContainer].
   Future<ApiResult<LeaveRequestsContainer>> getLeavesList();
+
+  /// Retrieves a list of crew members.
+  /// Returns a future of [ApiResult] containing a [CrewMembersContainer].
   Future<ApiResult<CrewMembersContainer>> getCrewMemberList();
 }
