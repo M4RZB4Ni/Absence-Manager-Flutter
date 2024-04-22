048d291423632a4155d005e2eddd0fd35099b87d
Marzbani <hamied.reza.m@gmail.com>
2024-04-22 11:44:53 +0330

    Docs: comments were added to the main_binding.dart class, which enhances its readability.
    Description: none
    
    Took 2 minutes

--- lib/app/di/main_binding.dart
+++ lib/app/di/main_binding.dart
@@ -7,22 +7,47 @@ import 'package:communere/domain/usecase/get_crew_members_usecase.dart';
 import 'package:communere/presentation/home/home_view_model.dart';
 import 'package:get/get.dart';
 
+/// The `MainBinding` class is responsible for setting up and binding all the necessary dependencies
+/// for the application using the GetX package.
+///
+/// It implements the `Bindings` interface which requires the definition of the `dependencies` method.
+/// This method will be called automatically by the GetX framework when the corresponding route is navigated to.
 class MainBinding implements Bindings {
+  /// Overrides the `dependencies` method to define how the dependencies should be created and injected.
+  ///
+  /// It uses the `Get.lazyPut` method to lazily instantiate the classes only when they are needed.
+  /// This is more memory efficient as opposed to eagerly creating instances that may not be immediately required.
   @override
   void dependencies() {
+    // Binds the `HomeViewModel` to the dependency injection system.
+    // It will find the required use cases (`GetAbsencesUseCase` and `GetCrewMembersUseCase`)
+    // and inject them into the `HomeViewModel`.
     Get.lazyPut<HomeViewModel>(() => HomeViewModel(
         Get.find<GetAbsencesUseCase>(), Get.find<GetCrewMembersUseCase>()));
 
+    // Binds the `GetAbsencesUseCase` to the dependency injection system.
+    // It will find the required repository (`AbsenceManagerRepository`)
+    // and inject it into the `GetAbsencesUseCase`.
     Get.lazyPut<GetAbsencesUseCase>(
-        () => GetAbsencesUseCase(Get.find<AbsenceManagerRepository>()));
+            () => GetAbsencesUseCase(Get.find<AbsenceManagerRepository>()));
 
+    // Binds the `GetCrewMembersUseCase` to the dependency injection system.
+    // It will find the required repository (`AbsenceManagerRepository`)
+    // and inject it into the `GetCrewMembersUseCase`.
     Get.lazyPut<GetCrewMembersUseCase>(
-        () => GetCrewMembersUseCase(Get.find<AbsenceManagerRepository>()));
+            () => GetCrewMembersUseCase(Get.find<AbsenceManagerRepository>()));
 
+    // Binds the `AbsenceManagerRepository` to the dependency injection system.
+    // It will find the required data source (`AbsenceManagerDataSource`)
+    // and inject it into the `AbsenceManagerRepositoryImpl`.
     Get.lazyPut<AbsenceManagerRepository>(() =>
         AbsenceManagerRepositoryImpl(Get.find<AbsenceManagerDataSource>()));
 
+    // Binds the `AbsenceManagerDataSource` to the dependency injection system.
+    // It creates an instance of `AbsenceManagerLocalDataSource` which is presumably
+    // the local data handling implementation for absence management.
     Get.lazyPut<AbsenceManagerDataSource>(
-        () => AbsenceManagerLocalDataSource());
+            () => AbsenceManagerLocalDataSource());
   }
 }
+
