086c6589690b829f6969930f0bdbbf6f9fae2c8d
Marzbani <hamied.reza.m@gmail.com>
2024-04-22 11:42:33 +0330

    Feat: main binding has been developed as responsible for necessary dependencies
    Description: Because the application is one page based at this moment, MainBinding is enough for DI management, if a new feature is added we have to write per feature binding file and call each file into main_binding.dart file.
    
    Took 19 minutes

--- lib/app/di/main_binding.dart
+++ lib/app/di/main_binding.dart
@@ -1,9 +1,28 @@
-import 'package:get/get_instance/get_instance.dart';
-
+import 'package:communere/data/data_source/local/absence_manager_local_data_source.dart';
+import 'package:communere/data/repository/absence_manager_repository_impl.dart';
+import 'package:communere/domain/data_source/absence_manager_data_source.dart';
+import 'package:communere/domain/repository/absence_manager_repository.dart';
+import 'package:communere/domain/usecase/get_absences_usecase.dart';
+import 'package:communere/domain/usecase/get_crew_members_usecase.dart';
+import 'package:communere/presentation/home/home_view_model.dart';
+import 'package:get/get.dart';
 
 class MainBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<HomeViewModel>(() => HomeViewModel(
+        Get.find<GetAbsencesUseCase>(), Get.find<GetCrewMembersUseCase>()));
 
+    Get.lazyPut<GetAbsencesUseCase>(
+        () => GetAbsencesUseCase(Get.find<AbsenceManagerRepository>()));
+
+    Get.lazyPut<GetCrewMembersUseCase>(
+        () => GetCrewMembersUseCase(Get.find<AbsenceManagerRepository>()));
+
+    Get.lazyPut<AbsenceManagerRepository>(() =>
+        AbsenceManagerRepositoryImpl(Get.find<AbsenceManagerDataSource>()));
+
+    Get.lazyPut<AbsenceManagerDataSource>(
+        () => AbsenceManagerLocalDataSource());
   }
 }
