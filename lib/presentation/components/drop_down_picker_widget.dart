e3a0381b61f23b750ce40ce6e7909c1ecdc6f795
Marzbani <hamied.reza.m@gmail.com>
2024-04-23 21:36:43 +0330

    Docs: Comments for drop_down_picker_widget.dart have been added.
    Description: Comprehensive comments were added and considered for use in generating documentation.
    
    Took 10 minutes

--- lib/presentation/components/drop_down_picker_widget.dart
+++ lib/presentation/components/drop_down_picker_widget.dart
@@ -5,10 +5,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../app/extentions/typedefs.dart';
 
+/// A custom dropdown widget that allows users to select from a list of string options.
+///
+/// This widget is stateful to manage the selected option internally and to provide
+/// a callback for when the selection changes.
 class StringDropdownWidget extends StatefulWidget {
+
+  /// Callback function that is called when a new string is selected.
   final SingleStringValueCallback onStringSelected;
+
+  /// List of string options that the user can select from.
   final List<String> options;
 
+  /// Constructs a [StringDropdownWidget].
+  ///
+  /// Requires [onStringSelected] to be provided to handle selection changes,
+  /// and [options] to display the list of selectable items.
   const StringDropdownWidget({
     super.key,
     required this.onStringSelected,
@@ -19,9 +31,15 @@ class StringDropdownWidget extends StatefulWidget {
   StringDropdownWidgetState createState() => StringDropdownWidgetState();
 }
 
+/// The state class for [StringDropdownWidget].
+///
+/// It manages the selected option and provides a method to reset the selection.
 class StringDropdownWidgetState extends State<StringDropdownWidget> {
+
+  /// The currently selected option, if any.
   String? _selectedOption;
 
+  /// Resets the selected option to null and calls the selection callback.
   void _resetSelection() {
     setState(() {
       _selectedOption = null;
@@ -31,6 +49,8 @@ class StringDropdownWidgetState extends State<StringDropdownWidget> {
 
   @override
   Widget build(BuildContext context) {
+
+    // Builds the dropdown widget with styling and behavior.
     return Container(
       height: 35.spMin,
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -49,6 +69,7 @@ class StringDropdownWidgetState extends State<StringDropdownWidget> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          // Dropdown button to display the options.
           DropdownButtonHideUnderline(
             child: DropdownButton<String>(
               value: _selectedOption,
@@ -58,8 +79,7 @@ class StringDropdownWidgetState extends State<StringDropdownWidget> {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value,
-                      style: TextStyle(
-                          fontSize: 12.sp, color: AppColors.infoDark)),
+                      style:Theme.of(context).textTheme.titleMedium),
                 );
               }).toList(),
               onChanged: (String? newValue) {
@@ -73,6 +93,8 @@ class StringDropdownWidgetState extends State<StringDropdownWidget> {
                       TextStyle(fontSize: 12.sp, color: AppColors.primaryDark)),
             ),
           ),
+
+          // Clear button to reset the selection.
           if (_selectedOption != null)
             SizedBox(
               width: 18.w,
